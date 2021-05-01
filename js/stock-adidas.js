d32373aceb3c50bf27cfc03a705f37d532cf855e
sofia triana <sofia triana>
2021-04-30 21:29:56 -0500

    JS

---
+++ js/stock-adidas.js
@@ -0,0 +1,14 @@
+let productosAdidas = [
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+    {Talla: 36, prenda: 'Tennis', precio: 30},
+    {Talla: 37, prenda: 'Tennis', precio: 30},
+    {Talla: 38, prenda: 'Tennis', precio: 30},
+    {Talla: 39, prenda: 'Tennis', precio: 30},
+    {Talla: 40, prenda: 'Tennis', precio: 30},
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+    {Talla: 35, prenda: 'Tennis', precio: 30},
+]
