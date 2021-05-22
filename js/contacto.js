cea01293e7732a3e9435e4958c3108b119909eba
sofia triana <sofia triana>
2021-05-22 01:02:06 -0500

    proyecto

--- js/contacto.js
+++ js/contacto.js
@@ -1,23 +1,16 @@
+$( document ).ready(function() {
+    const APIURL = 'https://hp-api.herokuapp.com/api/characters' ; 
 
-const KEY = 'F8OpHcQX4zWWiea6boAd6QRvTtj1ZUc9'
-
-const llamarApi = async (busqueda)=>{
-    const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${ encodeURI(busqueda) }&limit=40`)
-    const {data} = await resp.json()
-    const arrayImgs = data.map(el => el.images.downsized_medium.url)
-    
-    $('.row').html('')
-    arrayImgs.forEach(img => {          
-        $('.row').append(`
-                    <li class="card col-3">
-                        <img src=${img} alt="" />
-                    </li>
-                `)
+    const infoPost = {"name":"Harry Potter","gender":"male","house":"Gryffindor"},
+ 
+    $("#sumbit").click(() => { 
+        $.ajax({
+            method: "GET",
+            url:  APIURL,
+            data: infoPost,
+            success: function(respuesta){
+                $("body").prepend(`<div>${respuesta.nombre}</div>`);
+            }
+        });
     });
-}
-
-
-$('#la').on('submit', (e)=>{
-    e.preventDefault()
-    llamarApi( $('input').val() )
-})
+});
